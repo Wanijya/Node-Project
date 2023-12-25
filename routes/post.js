5d121bb1436b6bba047d81db9e74e7ef2df85ea1
Wanijya <wanijyabisen@gmail.com>
2023-12-26 03:31:42 +0530

    kuchha nya

---
+++ routes/post.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const postShema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  title: String,
+  description: String,
+  image: String,
+});
+
+
+module.exports = mongoose.model("post", postShema);
